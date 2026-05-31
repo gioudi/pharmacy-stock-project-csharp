8b20fc317461ad06581545848c59ca53f7bd9046
gioudi <sergiopenagos881@gmail.com>
2026-05-31 15:33:28 -0500

    Step 3 completed: Main Views created

---
+++ PharmacyStockApp/Views/IMainFormView.cs
@@ -0,0 +1,25 @@
+﻿using PharmacyStockApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyStockApp.Views
+{
+    public interface IMainFormView
+    {
+        string MedicineName { get; set; }
+        MedicineType SelectedType { get; set; }
+        string QuantityInput { get; set; }
+        Provider? SelectedProvider { get; set; }
+        bool IsPrimaryBranchChecked { get; set; }
+        bool IsSecondaryBranchChecked { get; set; }
+
+        event EventHandler ConfirmClicked;
+        event EventHandler DeleteClicked;
+
+        void ClearFields();
+        void ShowWarning(string message);
+        void HideView();
+        void ShowView();
+    }
+}

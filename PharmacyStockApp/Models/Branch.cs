cbe95a12287ea326fc0e417a3f60642a667925fc
gioudi <sergiopenagos881@gmail.com>
2026-05-31 13:40:38 -0500

    Step 2 completed: Models created

---
+++ PharmacyStockApp/Models/Branch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyStockApp.Models
+{
+    public enum Branch
+    {
+        Primaria, 
+        Secuandaria
+    }
+    public static class BranchExtensions
+    {
+        public static string GetAddress(this Branch branch)
+        {
+            return branch switch
+            {
+                Branch.Primaria => "Calle de la Rosa n.28",
+                Branch.Secuandaria => "Calle Alcazabilla n. 3",
+                _ => "Dirección no encontraron"
+            };
+        }
+    }
+}
---
+++ PharmacyStockApp/Models/MedicineOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyStockApp.Models
+{
+    public class MedicineOrder
+    {
+        public string MedicineName { get; set; }
+        public MedicineType Type { get; set; }
+        public int Quantity { get; set; }
+        public Provider ProviderCompany { get; set; }
+        public Branch  TargetBranch { get; set; }
+
+
+
+        public MedicineOrder(string medicineName, MedicineType type, int quantity, Provider providerCompany, Branch targetBranch) { 
+        
+            MedicineName = medicineName;
+            Type = type;
+            Quantity = quantity;
+            ProviderCompany = providerCompany;
+            TargetBranch = targetBranch;
+        }
+
+    }
+}
